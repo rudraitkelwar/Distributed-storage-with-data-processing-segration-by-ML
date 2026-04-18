81024f9c0c2c4b1f3250b7538d7dc158c3f43cc5
Rudra Itkelwar <rudraitkelwar@mac.mynetworksettings.com>
2026-04-17 21:07:08 -0400

    script to load data in lancedb

--- LanceDB_setup/LanceDB_setup.sh
+++ LanceDB_setup/LanceDB_setup.sh
@@ -1,3 +1,8 @@
 sudo yum update
 sudo yum install -y python3 python3-pip
 pip install "lancedb>=0.8.0" "numpy>=1.26" "pydantic>=2"
+
+
+sudo dnf install -y python39 python39-pip   # or yum depending on your distro
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install "lancedb" "numpy>=1.26" "pydantic>=2"
